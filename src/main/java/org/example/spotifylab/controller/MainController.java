2e2d32b5aa5a65c1f63be8b0d751f967f9ece510
Eva <eva@example.com>
2026-09-11 10:34:10 -0400

    Connect FXML to controller

---
+++ src/main/java/org/example/spotifylab/controller/MainController.java
@@ -0,0 +1,8 @@
+package org.example.spotifylab.controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class MainController {
+    @FXML
+    private TextField champRecherche;
+}
