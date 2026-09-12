f349ecad6e12c0092b7e875fce6e219352bf50c7
Eva <eva@example.com>
2026-09-12 12:10:07 -0400

    Ajout fix temporaire pour tester programme

--- src/main/java/org/example/spotifylab/service/BenchmarkService.java
+++ src/main/java/org/example/spotifylab/service/BenchmarkService.java
@@ -6,17 +6,39 @@ import org.example.spotifylab.model.Chanson;
 import java.util.ArrayList;
 import java.util.List;
 
+
+// Eva: Fix temporaire pour tester programme
+
+import java.util.Comparator;
+
 public class BenchmarkService {
     public List<ResultatBenchmark> mesurer(List<Chanson> chansons,
                                            List<AlgorithmeTri<Chanson>> algorithmes,
-                                           CritereTri critere) {
+                                           Comparator<Chanson> comparateur) {
         List<ResultatBenchmark> resultats = new ArrayList<>();
         for (AlgorithmeTri<Chanson> algorithme : algorithmes) {
             long debut = System.nanoTime();
-            algorithme.trier(chansons, critere.getComparateur());
+            algorithme.trier(chansons, comparateur);
             long duree = System.nanoTime() - debut;
             resultats.add(new ResultatBenchmark(algorithme.nom(), algorithme.complexiteTheorique(), duree));
         }
         return resultats;
     }
 }
+
+// Code de Marc-André:
+
+//public class BenchmarkService {
+//    public List<ResultatBenchmark> mesurer(List<Chanson> chansons,
+//                                           List<AlgorithmeTri<Chanson>> algorithmes,
+//                                           CritereTri critere) {
+//        List<ResultatBenchmark> resultats = new ArrayList<>();
+//        for (AlgorithmeTri<Chanson> algorithme : algorithmes) {
+//            long debut = System.nanoTime();
+//            algorithme.trier(chansons, critere.getComparateur());
+//            long duree = System.nanoTime() - debut;
+//            resultats.add(new ResultatBenchmark(algorithme.nom(), algorithme.complexiteTheorique(), duree));
+//        }
+//        return resultats;
+//    }
+//}
