f30bb025c22fbffbcdef7011d45b280cd73f5671
Marc-Andre Dufour <bleeband.dev@gmail.com>
2026-09-13 11:44:12 -0400

    Nettoyer le service de benchmark

--- src/main/java/org/example/spotifylab/service/BenchmarkService.java
+++ src/main/java/org/example/spotifylab/service/BenchmarkService.java
@@ -4,12 +4,8 @@ import org.example.spotifylab.algorithmes.AlgorithmeTri;
 import org.example.spotifylab.model.Chanson;
 
 import java.util.ArrayList;
-import java.util.List;
-
-
-// Eva: Fix temporaire pour tester programme
-
 import java.util.Comparator;
+import java.util.List;
 
 public class BenchmarkService {
     public List<ResultatBenchmark> mesurer(List<Chanson> chansons,
@@ -31,20 +27,3 @@ public class BenchmarkService {
         return mesurer(chansons, algorithmes, critere.getComparateur());
     }
 }
-
-// Code de Marc-André:
-
-//public class BenchmarkService {
-//    public List<ResultatBenchmark> mesurer(List<Chanson> chansons,
-//                                           List<AlgorithmeTri<Chanson>> algorithmes,
-//                                           CritereTri critere) {
-//        List<ResultatBenchmark> resultats = new ArrayList<>();
-//        for (AlgorithmeTri<Chanson> algorithme : algorithmes) {
-//            long debut = System.nanoTime();
-//            algorithme.trier(chansons, critere.getComparateur());
-//            long duree = System.nanoTime() - debut;
-//            resultats.add(new ResultatBenchmark(algorithme.nom(), algorithme.complexiteTheorique(), duree));
-//        }
-//        return resultats;
-//    }
-//}
